046e61f22173114f2f651d82af1cedb85e583aa0
Hemant Agrawal <ha.hemantagrawal@gmail.com>
2024-06-27 15:47:38 +0000

    Added bitwise manipulation to handle issue for values less than 16

--- src/rgbToHex.ts
+++ src/rgbToHex.ts
@@ -1,3 +1,3 @@
 export function rgbToHex([red = 0, green = 0, blue = 0] = []) {
-  return `#${((red << 16) | (green << 8) | blue).toString(16)}`;
+  return `#${((1 << 24) | (red << 16) | (green << 8) | blue).toString(16).slice(1)}`;
 }
